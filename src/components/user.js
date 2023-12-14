89264e35f80399808c23cc33a5d728d369795341
jaylimbachiya43 <109168679+jaylimbachiya43@users.noreply.github.com>
2023-12-14 21:03:01 +0530

    new change

--- src/components/user.js
+++ src/components/user.js
@@ -12,5 +12,17 @@ export const users = [
     photo: "modi.png",
     email: "john@example.com",
   },
+  {
+    id: 3,
+    name: "parth",
+    photo: "parth.jpg",
+    email: "john@example.com",
+  },
+  {
+    id: 4,
+    name: "sahil",
+    photo: "sahil.jpg",
+    email: "john@example.com",
+  },
   // Add more users as needed
 ];
